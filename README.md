d3a44af1c4b4c9cbd829218c041433e6002c5411
tomcotter7 <thomascotter00@gmail.com>
2023-10-18 21:21:24 +0100

    Added initial RAG app

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# A GitHub repository for my notes on AI and ML.
+# Notes + RAG App
 
-Simply search for the topic using '/' on nvim. Easy to use.
+## notes.md
+
+All my notes on any topic I find interesting
+
+## rag/
+
+A simple web app to allow me to query ChatGPT with my notes as context.
+
+
+## TODO
+
+  - [ ] Add either a Weaviate DB connection or a in-memory vector store.
+  - [ ] Add a simple web interface to query the DB.
+  - [ ] Connect web interface & backend to return relevant notes
+  - [ ] Query ChatGPT with notes as context
---
+++ rag/preprocessing/markdown.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+HERE = Path(__file__).parent
+notes_file = HERE.parent.parent / "notes.md"
+
+
+def read_as_str(path: Path) -> str:
+    with open(path, "r") as f:
+        return f.read()
+
+
+def convert_to_chunks(text: str) -> list[str]:
+
+    headers = {2: "", 3: "", 4: ""}
+
+    contextualized_lines = []
+
+    for line in text.splitlines():
+        if line.startswith("#"):
+            level = line.count("#")
+            if level in headers:
+                headers[level] = line.replace("#", "").strip()
+
+        elif line.startswith("-") or len(line) == 0:
+            pass
+
+        else:
+            new_line = f"{headers[2]}: {headers[3]}: {headers[4]}: {line}"
+            contextualized_lines.append(new_line)
+
+    return contextualized_lines
+
+
+def load_and_convert() -> list[str]:
+    text = read_as_str(notes_file)
+    chunks = convert_to_chunks(text)
+    return chunks
