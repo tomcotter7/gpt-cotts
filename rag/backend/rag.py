df32839c98585a1fdf104ec370852d70bd952bbb
tomcotter7 <thomascotter00@gmail.com>
2023-10-31 07:09:15 +0000

    update notes

--- rag/backend/rag.py
+++ rag/backend/rag.py
@@ -10,15 +10,17 @@ class RAG:
 
     def reset_context(self) -> None:
         self.mqh.reset_context()
+        self.query_calls = 0
 
-    def query(self, input_query: str) -> tuple[str, list[str]]:
+    def reduce_context_size(self) -> None:
+        self.mqh.reduce_context_size()
+
+    def query(self, input_query: str, use_rag: bool) -> tuple[str, list[str]]:
         chunks = []
-        if self.query_calls == 0:
+        if self.query_calls == 0 and use_rag:
             chunks = query_weaviate(input_query, "localhost")
         model_response = self.mqh.query(input_query, chunks)
         self.query_calls += 1
-        if self.query_calls > 2:
-            self.reset_context()
-            self.query_calls = 0
-
+        if self.query_calls == 4:
+            self.reduce_context_size()
         return model_response, chunks
