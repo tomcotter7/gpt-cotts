483950e7ceae53fd4bff249ed4f63750e5b935b3
tomcotter7 <thomascotter00@gmail.com>
2023-12-21 08:45:10 +0000

    added a navbar

--- gpt-cotts-frontend/app/page.js
+++ gpt-cotts-frontend/app/page.js
@@ -17,18 +17,10 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex m-2">
-        <div>
-          <h1 className="text-4xl text-black">
-            <u><b>gpt-cotts</b></u>
-          </h1>
-          <h2 className="text-fuchsia-500"> skill issues? use me </h2>
-        </div>
-        <div className="ml-auto">
-          <Settings
-            onSettingsChange={handleSettingsChange}
+      <div className="flex justify-end m-3">
+        <Settings
+          onSettingsChange={handleSettingsChange}
           />
-        </div>
       </div>
       <Chat settings={settings}/>
     </>
