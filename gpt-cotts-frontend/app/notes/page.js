c7e5e4703556585b0597bef04de36727df5d6f08
tomcotter7 <thomascotter00@gmail.com>
2023-12-27 15:35:00 +0000

    working edit button

--- gpt-cotts-frontend/app/notes/page.js
+++ gpt-cotts-frontend/app/notes/page.js
@@ -5,24 +5,64 @@ import rehypeKatex from 'rehype-katex'
 import 'katex/dist/katex.min.css'
 import { useState, useEffect } from 'react'
 
-function Section({ title, content }) {
+function EditingSection({ title, content, id }) {
+
+  const height = content.split('\n').length * 30
+
+  return (
+    <div className="flex flex-col" id={id}>
+      <input
+        className="border border-2 rounded px-2"
+        defaultValue={title}
+      />
+      <textarea
+        className='border border-2 rounded px-2'
+        style={{ height: `${height}px` }}
+        defaultValue={content}
+      />
+    </div>
+  )
+}
+
+function MarkdownSection({ title, content }) {
+  return (
+    <div>
+      <h1 className="text-fuchsia-500 text-center"><b> {title} </b></h1>
+      <Markdown remarkPlugins={remarkMath} rehypePlugins={rehypeKatex}>
+        {content}
+      </Markdown>
+    </div>
+  )
+}
+
+function Section({ title, content, onSectionSave, id }) {
   
   const [editing, setEditing] = useState(false)
+  const [newTitle, setNewTitle] = useState(title)
+  const [newContent, setNewContent] = useState(content)
+
+  const onEditButtonClick = () => {
+    if (editing) {
+      const editingDiv = document.getElementById(id)
+      setNewTitle(editingDiv.children[0].value)
+      setNewContent(editingDiv.children[1].value)
+      console.log(title)
+      onSectionSave({ [title]: [editingDiv.children[0].value, editingDiv.children[1].value] })
+    }
+    setEditing(!editing)
+  }
 
   return (
     <div>
       <div className="flex justify-end">
         <button
           className="px-4 mr-2 mt-2 bg-fuchsia-500 hover:bg-fuchsia-400 rounded border border-2 text-white"
-          onClick={() => setEditing(!editing)}
+          onClick={onEditButtonClick}
         >
-          edit
+        {editing ? 'Save' : 'Edit'}
         </button>
       </div>
-      <h1 className="text-fuchsia-500 text-center"><b> {title} </b></h1>
-      <Markdown remarkPlugins={remarkMath} rehypePlugins={rehypeKatex}>
-          {content}
-      </Markdown>
+      {editing ? <EditingSection id={id} title={newTitle} content={newContent}/> : <MarkdownSection title={newTitle} content={newContent} />}
     </div>
   )
 }
@@ -44,10 +84,16 @@ export default function Notes() {
 
       const data = await response.json()
       setNotes(data)
-      console.log(data)
     }
     getNotes()
   }, [])
+
+  const onSectionSave = (updatedSection) => {
+    var newNotes = notes
+    delete newNotes[Object.keys(updatedSection)[0]]
+    newNotes[Object.values(updatedSection)[0][0]] = Object.values(updatedSection)[0][1]
+    setNotes(newNotes)
+  }
 
   return (
     <div className="m-2">
@@ -55,7 +101,7 @@ export default function Notes() {
       <div className="flex flex-col border items-center">
         {Object.entries(notes).map(([key, value]) => (
           <div key={Date.now() + key} className="border w-full text-center prose max-w-none">
-            <Section key={key} title={key} content={value} />
+            <Section key={key} id={key} title={key} content={value} onSectionSave={(updatedSection) => onSectionSave(updatedSection)}/>
           </div>
         ))}
       </div>
