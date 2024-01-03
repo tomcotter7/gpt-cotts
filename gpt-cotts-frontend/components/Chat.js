088e1f9c5dbe292bb97f63fed838fba576effadd
tomcotter7 <thomascotter00@gmail.com>
2024-01-03 08:02:22 +0000

    working dockerfile

--- gpt-cotts-frontend/components/Chat.js
+++ gpt-cotts-frontend/components/Chat.js
@@ -74,9 +74,9 @@ export default function Chat({settings}) {
     stop.current = false
     let stream;
     if (settings.rag) {
-      stream =  await sendMessage(message, settings.animalese, "http://localhost:8000/rag")
+      stream =  await sendMessage(message, settings.animalese, "http://13.48.5.66:8000/rag")
     } else {
-      stream = await sendMessage(message, settings.animalese, "http://localhost:8000/llm")
+      stream = await sendMessage(message, settings.animalese, "http://13.48.5.66:8000/llm")
     }
     
     let response = ""
@@ -114,7 +114,7 @@ export default function Chat({settings}) {
   function onClearButtonClick() {
     stop.current = true
     setChats([])
-    pingServer("http://localhost:8000/clear")
+    pingServer("http://13.48.5.66:8000/clear")
   }
 
   if (chats.length === 0) {
