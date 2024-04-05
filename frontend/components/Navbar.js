77c20502637e5507f4e76efe9e1e9f10a33e0a4e
tomcotter7 <thomascotter00@gmail.com>
2024-04-05 14:14:00 +0100

    removing StopSIgn

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -3,7 +3,6 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
 import axios from 'axios';
-import { StopSign } from '@/utils/svgs'
 
 const LoginLink = () => {
 
