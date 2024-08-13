fe9bbe19890ae1d364a1ec9ed77dede821b75d4e
tomcotter7 <thomascotter00@gmail.com>
2024-08-13 20:26:07 +0100

    adding missing files

---
+++ frontend/app/auth/signin/page.js
@@ -0,0 +1,22 @@
+"use client"
+import { signIn, useSession } from 'next-auth/react'
+
+
+export default function SignIn() {
+    const { data: session, status } = useSession()
+
+    const not_expired = session && new Date(session.expires).getTime() > Date.now()
+    if (status === "authenticated" && not_expired) {
+        window.location.href = "/"
+    }
+
+    return (
+        <>
+            <div className="flex justify-center mt-10">
+                <button className="mt-2 border border-spearmint hover:bg-spearmint hover:border-spearmint-dark font-bold py-2 px-4 rounded-md justify-center flex" onClick={() => signIn("google", { callbackUrl: process.env.NEXTAUTH_CALLBACK_URL })}>
+                    <img className="w-6 h-6 mr-2" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" /> Sign in w/ Google
+                </button>
+            </div>
+        </>
+    )
+}
