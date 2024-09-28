b2be8f0cfad04d2bcc3bc0eebacfb70391dce7a6
tomcotter7 <thomascotter00@gmail.com>
2024-09-28 15:59:55 +0100

    updated tldraw to fix rendering issue

--- frontend/app/tldraw/page.js
+++ frontend/app/tldraw/page.js
@@ -1,23 +1,32 @@
 "use client"
 
+import { useState, useEffect } from 'react'
 import { Tldraw } from 'tldraw'
 import 'tldraw/tldraw.css'
 import { useSession } from 'next-auth/react'
 
 
 export default function TlDraw() {
-
+    
+    const [adjustedHeight, setAdjustedHeight] = useState('93vh')
     const { data: session, status } = useSession()
+
+    useEffect(() => {
+        const nav = document.getElementById('nav')
+        if (nav) {
+          const vh = (nav.offsetHeight / screen.height) * 100
+          setAdjustedHeight((95 - vh) + 'vh')
+        }
+
+    }, [])
 
     if (status === "loading") {
         return null
     }
 
     return (
-        <div className="tldraw__editor" style={{"height": "100vh"}}>
+        <div className="tldraw__editor m-1" style={{"height": adjustedHeight}}>
 			<Tldraw persistenceKey={session.user.email} />
 		</div>
 	)
 }
-
-
