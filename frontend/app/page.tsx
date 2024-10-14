ba4440b99f0376edbda77a0e1cba69436cef155b
tomcotter7 <thomascotter00@gmail.com>
2024-10-14 08:54:05 +0100

    Added note on release

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -31,6 +31,9 @@ export default function Home() {
     } else if (status === "authenticated") {
         return (
             <div className="flex justify-center" style={{height: adjustedHeight}}>
+            <div className="fixed top-45 right-2 bg-red-400 w-1/6 rounded shadow-md p-1">
+                <p className="text-white text-sm"> ! A major code rewrite was just released. If you have any issues, please let me know. ! </p>
+            </div>
                 <Chat />
             </div>
         )
